4894e1dab473c4fd619239c09e70be7482be753b
Ahmet Selim Kaya <askaya@etu.edu.tr>
2017-05-28 02:56:47 +0300

    Collision detection and coin collection are working now.

--- Assg1.cpp
+++ Assg1.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 #include <gl/glut.h>
 
+GLfloat hViewport = 600.0, wViewport = 500.0;
+GLfloat hTriangle = hViewport / 25.0, wTriangle = wViewport / 25.0;
+GLfloat xTriangle = wViewport / 2, yTriangle = hTriangle;
+
 typedef struct{
 	int x;
 	int y; // One of coordinate of a vehicle which is our reference point for drawing.
@@ -13,11 +17,16 @@ typedef struct{
 	int direction; // If 1, the vehicle goes +x direction. If 0, it goes -x direction.
 } Vehicle;
 
-GLfloat hViewport = 600.0, wViewport = 500.0;
-GLfloat hTriangle = hViewport / 25.0, wTriangle = wViewport / 25.0;
-GLfloat xTriangle = wViewport / 2, yTriangle = hTriangle;
+typedef struct {
+	int x;
+	int y; 
+	int r = hTriangle / 2;
+	int duration = 300;
+} Circle;
+
 
 std::vector<std::vector<Vehicle>> vehicleList (19);
+std::vector<Circle> coins;
 
 //This variable is a flag for triangle's direction. If it is 1, it means the triangle is going up. If it is -1, it means down.
 int isUpper = 1;
@@ -167,7 +176,7 @@ void drawRoad(){
 }
 
 void drawCars() {
-	glColor3f(0.0, 1.0, 0.0);
+	glColor3f(0.6, 0.6, 0.6);
 
 	for (auto& row : vehicleList) {
 		for (auto& col : row) {
@@ -175,19 +184,19 @@ void drawCars() {
 
 				if (col.direction) { // for positive direction
 					glBegin(GL_POLYGON);
-					glVertex2f(col.x, col.y);
-					glVertex2f(col.x - hTriangle, col.y);
-					glVertex2f(col.x - hTriangle, col.y - hTriangle);
-					glVertex2f(col.x, col.y - hTriangle);
+						glVertex2f(col.x, col.y);
+						glVertex2f(col.x - hTriangle, col.y);
+						glVertex2f(col.x - hTriangle, col.y - hTriangle);
+						glVertex2f(col.x, col.y - hTriangle);
 					glEnd();
 				}
 
 				else { // for negative direction
 					glBegin(GL_POLYGON);
-					glVertex2f(col.x, col.y);
-					glVertex2f(col.x + hTriangle, col.y);
-					glVertex2f(col.x + hTriangle, col.y - hTriangle);
-					glVertex2f(col.x, col.y - hTriangle);
+						glVertex2f(col.x, col.y);
+						glVertex2f(col.x + hTriangle, col.y);
+						glVertex2f(col.x + hTriangle, col.y - hTriangle);
+						glVertex2f(col.x, col.y - hTriangle);
 					glEnd();
 				}
 			}
@@ -195,19 +204,19 @@ void drawCars() {
 			else { // means this vehicle is a rectangle
 				if (col.direction) { // for positive direction
 					glBegin(GL_POLYGON);
-					glVertex2f(col.x, col.y);
-					glVertex2f(col.x - 2 * hTriangle, col.y);
-					glVertex2f(col.x - 2 * hTriangle, col.y - hTriangle);
-					glVertex2f(col.x, col.y - hTriangle);
+						glVertex2f(col.x, col.y);
+						glVertex2f(col.x - 2 * hTriangle, col.y);
+						glVertex2f(col.x - 2 * hTriangle, col.y - hTriangle);
+						glVertex2f(col.x, col.y - hTriangle);
 					glEnd();
 				}
 
 				else { // for negative direction
 					glBegin(GL_POLYGON);
-					glVertex2f(col.x, col.y);
-					glVertex2f(col.x + 2 * hTriangle, col.y);
-					glVertex2f(col.x + 2 * hTriangle, col.y - hTriangle);
-					glVertex2f(col.x, col.y - hTriangle);
+						glVertex2f(col.x, col.y);
+						glVertex2f(col.x + 2 * hTriangle, col.y);
+						glVertex2f(col.x + 2 * hTriangle, col.y - hTriangle);
+						glVertex2f(col.x, col.y - hTriangle);
 					glEnd();
 				}
 			}
@@ -215,71 +224,134 @@ void drawCars() {
 	}
 }
 
-void drawVehicles() {
-	glColor3f(0.0, 1.0, 0.0);
-	
-	for (int index = 0; index < vehicleList.size(); ++index) {
-		
-		for (int j = 0; j < vehicleList[index].size(); j++) {
-			if (vehicleList[index][j].type) { // means this vehicle is a square
-				
-				if (vehicleList[index][j].direction) { // for positive direction
-					glBegin(GL_POLYGON);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x-hTriangle, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x - hTriangle, vehicleList[index][j].y-hTriangle);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y - hTriangle);
-					glEnd();
-				}
-				
-				else { // for negative direction
-					glBegin(GL_POLYGON);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x + hTriangle, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x + hTriangle, vehicleList[index][j].y - hTriangle);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y - hTriangle);
-					glEnd();
-				}
-			}
-			
-			else { // means this vehicle is a rectangle
-				if (vehicleList[index][j].direction) { // for positive direction
-					glBegin(GL_POLYGON);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x - 2 * hTriangle, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x - 2 * hTriangle, vehicleList[index][j].y - hTriangle);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y - hTriangle);
-					glEnd();
-				}
-				
-				else { // for negative direction
-					glBegin(GL_POLYGON);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x + 2 * hTriangle, vehicleList[index][j].y);
-						glVertex2f(vehicleList[index][j].x + 2 * hTriangle, vehicleList[index][j].y - hTriangle);
-						glVertex2f(vehicleList[index][j].x, vehicleList[index][j].y - hTriangle);
-					glEnd();
-				}
-			}
-		}
+void drawCircle(float cx, float cy, float r)
+{
+	int num_segments = 90;
+	glBegin(GL_POLYGON);
+	for (int i = 0; i < num_segments; i++)
+	{
+		float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);//get the current angle
+
+		float x = r * cosf(theta);//calculate the x component
+		float y = r * sinf(theta);//calculate the y component
+
+		glVertex2f(x + cx, y + cy);//output vertex
+
 	}
+	glEnd();
+}
+
+void drawCoins() {
+	glColor3f(1.0, 1.0, 0.0);
+	for (auto& coin : coins) 
+		drawCircle(coin.x, coin.y, coin.r);
 }
 
 static void up() {
-	int count = 2;
-	for (auto& row : vehicleList) {
+	int count = 2; // this count is the # of road. because of the first one is a safe place, we started with 2
+	for (auto& road : vehicleList) {
 		if (count == 6 || count == 10 || count == 15 || count == 20) // this count is for determining the # of road. This numbers are safe places, not road.
 			count++; // Then, we increase the count for skipping safe places.
 
-		for (auto& col : row) {
-
-			if (col.direction)
-				col.x += (count % 5) * 5 + 10;
+		for (auto& vehicle : road) { // updating vehicles' places
+			if (vehicle.direction)
+				vehicle.x += (count % 3) * 2 + 3;
 			else
-				col.x -= (count % 3) * 2 + 10;
+				vehicle.x -= (count % 3) * 3 + 2;
+
+			// this part is for determining if a vehicle crushes to our triangle or not
+
+			if (isUpper == 1) { // means our triangle goes up
+				if (vehicle.y == yTriangle && vehicle.x >= xTriangle - wTriangle / 2 && vehicle.x <= xTriangle + wTriangle/2)
+					exit(0); // ADD A CLOSING METHOD HERE
+
+				else {
+					if (vehicle.direction) { // +x
+						if (vehicle.type) { // square
+							if (vehicle.y == yTriangle && xTriangle <= vehicle.x && xTriangle >= vehicle.x - hTriangle)
+								exit(0);
+						}
+						else { // rectangle
+							if (vehicle.y == yTriangle && xTriangle <= vehicle.x && xTriangle >= vehicle.x - 2 * hTriangle)
+								exit(0);
+						}
+					}
+					else { // -x
+						if (vehicle.type) { // square
+							if (vehicle.y == yTriangle && xTriangle >= vehicle.x && xTriangle <= vehicle.x + hTriangle)
+								exit(0);
+						}
+						else { // rectangle
+							if (vehicle.y == yTriangle && xTriangle >= vehicle.x && xTriangle <= vehicle.x + 2 * hTriangle)
+								exit(0);
+						}
+					}
+				}
+			}
+			else { // means our triangle goes down
+				if (vehicle.y == yTriangle + hTriangle && vehicle.x >= xTriangle - wTriangle / 2 && vehicle.x <= xTriangle + wTriangle / 2)
+					exit(0);
+
+				else {
+					if (vehicle.direction) { // +x
+						if (vehicle.type) { // square
+							if (vehicle.y == yTriangle + hTriangle && xTriangle <= vehicle.x && xTriangle >= vehicle.x - hTriangle)
+								exit(0);
+						}
+						else { // rectangle
+							if (vehicle.y == yTriangle + hTriangle && xTriangle <= vehicle.x && xTriangle >= vehicle.x - 2 * hTriangle)
+								exit(0);
+						}
+					}
+					else { // -x
+						if (vehicle.type) { // square
+							if (vehicle.y == yTriangle + hTriangle && xTriangle >= vehicle.x && xTriangle <= vehicle.x + hTriangle)
+								exit(0);
+						}
+						else { // rectangle
+							if (vehicle.y == yTriangle + hTriangle && xTriangle >= vehicle.x && xTriangle <= vehicle.x + 2 * hTriangle)
+								exit(0);
+						}
+					}
+				}
+			}
 		}
 
-		if (!(rand() % 35)) {
+		std::vector<Vehicle>::iterator it = road.begin(); // with this iterator, we will delete the vehicles which exit the screen
+
+		while (it != road.end()) { // iteration over the road
+			Vehicle vhc = *it;
+			if (vhc.direction) { // means this vehicle goes +x way
+				if (vhc.type) { // means this is a square
+					if (vhc.x - hTriangle >= wViewport) // means this square is out of road. we can delete it
+						it = road.erase(it);
+					else
+						++it; // if not, we skip at this time
+				}
+				else { // means this vehicle is a rectangle
+					if (vhc.x - 2 * hTriangle >= wViewport) // means this square is out of road. we can delete it
+						it = road.erase(it);
+					else
+						++it; // if not, we skip at this time
+				}
+			}
+			else { // means this vehicle goes -x way
+				if (vhc.type) { // means this is a square
+					if (vhc.x + hTriangle <= 0) // means this square is out of road. we can delete it
+						it = road.erase(it);
+					else
+						++it; // if not, we skip at this time
+				}
+				else { // means this vehicle is a rectangle
+					if (vhc.x + 2 * hTriangle <= 0) // means this square is out of road. we can delete it
+						it = road.erase(it);
+					else
+						++it; // if not, we skip at this time
+				}
+			}
+		}
+
+		if (!(rand() % 100)) { // adding new vehicle to the road
 			Vehicle v;
 			if (count % 2) { // If the # of road is a odd number, we assign its direction +x.
 				v.direction = 1;
@@ -296,103 +368,45 @@ static void up() {
 
 			else v.type = 0;
 
-			row.push_back(v);
+			road.push_back(v);
 		}
 		count++;
 	}
-}
 
-static void update() {
-	int count = 1;
-	for (int index = 0; index < vehicleList.size(); ++index) {
-		if (count == 6 || count == 10 || count == 15 || count == 20) // this count is for determining the # of road. This numbers are safe places, not road.
-			count++; // Then, we increase the count for skipping safe places.
+	for (int i = 0; i < coins.size(); i++) { // every single coin has a duration. when duration ends, the coin will disappear from the screen
+		coins[i].duration--;
+		if (!coins[i].duration)
+			coins.erase(coins.begin() + i);
+	}
 
-		for (int j = 0; j < vehicleList[index].size(); j++) {
-			vehicleList[index][j];
+	std::vector<Circle>::iterator it = coins.begin(); // this is an iteration for collecting coins. if the coins center overlaps with our triangle, it means we collected this coin
 
-			if (vehicleList[index][j].direction)
-				vehicleList[index][j].x += 2;
+	while (it != coins.end()) {
+		Circle c = *it;
+		if (c.x <= (xTriangle + (wTriangle / 2)) && c.x >= (xTriangle - (wTriangle / 2))) {
+			if (isUpper == 1 && c.y <= yTriangle && c.y >= yTriangle - hTriangle)
+				it = coins.erase(it);
+			else if (isUpper == -1 && c.y >= yTriangle && c.y <= yTriangle + hTriangle)
+				it = coins.erase(it);
 			else
-				vehicleList[index][j].direction -= 2;
+				++it;
 		}
-
-		if (!(rand() % 5)) {
-			Vehicle v;
-			if (count % 2) { // If the # of road is a odd number, we assign its direction +x.
-				v.direction = 1;
-				v.x = 0;
-				v.y = count * hTriangle;
-			}
-			else {
-				v.direction = 0;
-				v.x = wViewport;
-				v.y = count * hTriangle;
-			}
-
-			if (rand() % 2) v.type = 1;
-
-			else v.type = 0;
-
-			vehicleList[index].push_back(v);
-		}
+		else ++it;
+	}
+		
+	if (!(rand() % 100)) { // generating a coin randomly
+		Circle c;
+		c.x = rand() % 476 + 12;
+		c.y = (1 + (2 * rand()) % 50)*(hTriangle / 2);
+		coins.push_back(c);
 	}
 }
 
-static void updateVehicles() {
-	std::cout << "Working\n";
-	int count = 1;
-	//for (int index = 0; index < vehicleList.size(); ++index)
-	for (std::vector<std::vector<Vehicle>>::iterator listIterator = vehicleList.begin(); listIterator != vehicleList.end(); ++listIterator) {
-		std::vector<Vehicle> listTemp = *listIterator;
-		count++;
-		std::cout << count << std::endl;
-		std::cout << "Size ";
-		std::cout << listTemp.size() << std::endl;
-
-		if (count == 6 || count == 10 || count == 15 || count == 20) // this count is for determining the # of road. This numbers are safe places, not road.
-			count++; // Then, we increase the count for skipping safe places.
-
-		for (std::vector<Vehicle>::iterator it = listTemp.begin(); it != listTemp.end(); ++it) {
-			Vehicle tmp = *it;
-
-			if (tmp.direction)
-				tmp.x += 2;
-			else
-				tmp.direction -= 2;
-		}
-
-		if (!(rand() % 5)) {
-			std::cout << "Girdi" << std::endl;
-			Vehicle v;
-			if (count % 2) { // If the # of road is a odd number, we assign its direction +x.
-				v.direction = 1;
-				v.x = 0;
-				v.y = count * hTriangle;
-			}
-			else {
-				v.direction = 0;
-				v.x = wViewport;
-				v.y = count * hTriangle;
-			}
-
-			if (rand() % 2) v.type = 1; 
-			
-			else v.type = 0;
-
-			listTemp.push_back(v);
-
-			std::cout << "Eklendikten sonra ";
-			std::cout << listTemp.size() << std::endl;
-		}
-	}
-
-}
 
 void myTimer(int val) {
 	up();
 	glutPostRedisplay();
-	glutTimerFunc(500, myTimer, 0);
+	glutTimerFunc(50, myTimer, val);
 }
 
 void myinit(void){
@@ -448,6 +462,7 @@ void myDisplay(void) {
 	drawRoad();
 	drawTriangle();
 	drawCars();
+	drawCoins();
 	glFlush();
 }
 
